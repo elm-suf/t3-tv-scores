2a783600bf970748d4e92720ec921ec236539996
elm-suf <elm.sufian@gmail.com>
2024-11-29 16:58:59 +0100

    first component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+import { Button } from "~/components/ui/button";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
@@ -7,9 +7,8 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-
-        
         <h1>t3-tv-scores</h1>
+        <Button>Click me</Button>
       </main>
     </HydrateClient>
   );
